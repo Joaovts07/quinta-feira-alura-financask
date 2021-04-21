85db229222a601f2b4c4379f8be6c1359580cc93
joao <joakim.vts@gmail.com>
2021-04-21 11:18:48 -0300

    firebase modulo koin

--- app/src/main/java/br/com/alura/financask/di/AppModules.kt
+++ app/src/main/java/br/com/alura/financask/di/AppModules.kt
@@ -4,8 +4,11 @@ import androidx.room.Room
 import br.com.alura.financask.dao.TransacaoDAO
 import br.com.alura.financask.database.AppDatabase
 import br.com.alura.financask.repository.DatabaseDataSource
+import br.com.alura.financask.repository.FirebaseAuthRepository
 import br.com.alura.financask.repository.TransacaoRepository
+import br.com.alura.financask.ui.viewmodel.LoginViewModel
 import br.com.alura.financask.ui.viewmodel.TransacaoViewModel
+import com.google.firebase.auth.FirebaseAuth
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -30,17 +33,31 @@ val repositoryModule = module {
     single<TransacaoRepository> {
         DatabaseDataSource(get())
     }
+    single<FirebaseAuthRepository> {
+        FirebaseAuthRepository(get())
+    }
+
 }
 
 val viewModelModule = module {
     viewModel<TransacaoViewModel> {
         TransacaoViewModel(get())
     }
+    viewModel<LoginViewModel> {
+        LoginViewModel(get())
+    }
+}
+
+val firebaseModule = module {
+    single <FirebaseAuth> {
+        FirebaseAuth.getInstance()
+    }
 }
 val appModules = listOf(
         dbModule,
         daoModule,
         repositoryModule,
-        viewModelModule
+        viewModelModule,
+        firebaseModule
 
 )
