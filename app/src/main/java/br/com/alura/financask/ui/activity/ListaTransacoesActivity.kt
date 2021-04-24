b30f44cc5e46b0a8816ba159280e13141fd09dbf
joao <joakim.vts@gmail.com>
2021-04-23 23:17:47 -0300

    setunavcontroler para buttomnavigator

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,14 +2,21 @@ package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import br.com.alura.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
+    private val navControler by lazy {
+        findNavController(R.id.main_activity_nav_host)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+        main_activity_bottom_navigation.setupWithNavController(navControler)
     }
 
 
