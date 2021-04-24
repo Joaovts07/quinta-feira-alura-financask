0c704151820268b6fcdb458a212ca254cd8c7975
joao <joakim.vts@gmail.com>
2021-04-23 23:38:14 -0300

    criacao da basefragment

---
+++ app/src/main/java/br/com/alura/financask/ui/fragment/BaseFragment.kt
@@ -0,0 +1,29 @@
+package br.com.alura.financask.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.alura.financask.R
+import br.com.alura.financask.ui.viewmodel.LoginViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
+
+
+open class BaseFragment : Fragment() {
+
+    private val loginViewModel: LoginViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        isLoged()
+    }
+
+    private fun isLoged() {
+        if (loginViewModel.naoEstaLogado()) {
+            gotToLogin()
+        }
+    }
+
+    protected fun gotToLogin() {
+        findNavController().navigate(R.id.action_listaTransacoesFragment_to_loginFragment)
+    }
+}
